9b5478564dec601f7b3a681d3f4a4e5df6b4fc40
Andreu Zaitsev <zaytssss@mail.ru>
2024-03-08 14:12:53 +0100

    fix unit tests

--- persistent-cookie-jar-okhttp/src/test/java/com/andreuzaitsev/persistentcookiejar/PersistentCookieJarTestWithTestDoubles.java
+++ persistent-cookie-jar-okhttp/src/test/java/com/andreuzaitsev/persistentcookiejar/PersistentCookieJarTestWithTestDoubles.java
@@ -2,17 +2,13 @@ package com.andreuzaitsev.persistentcookiejar;
 
 import com.andreuzaitsev.persistentcookiejar.cache.CookieCache;
 import com.andreuzaitsev.persistentcookiejar.persistence.CookiePersistor;
-
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
 import okhttp3.Cookie;
-
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.atLeast;
@@ -34,9 +30,7 @@ public class PersistentCookieJarTestWithTestDoubles {
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, cookiePersistor);
         List<Cookie> responseCookies = Collections.singletonList(TestCookieCreator.createPersistentCookie(false));
 
-
         persistentCookieJar.saveFromResponse(TestCookieCreator.DEFAULT_URL, responseCookies);
-
 
         ArgumentCaptor<Collection<Cookie>> cookieCacheArgCaptor = ArgumentCaptor.forClass(Collection.class);
         verify(cookieCache, atLeastOnce()).addAll(cookieCacheArgCaptor.capture());
@@ -54,9 +48,7 @@ public class PersistentCookieJarTestWithTestDoubles {
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, cookiePersistor);
         final List<Cookie> responseCookies = Collections.singletonList(TestCookieCreator.createNonPersistentCookie());
 
-
         persistentCookieJar.saveFromResponse(TestCookieCreator.DEFAULT_URL, responseCookies);
-
 
         ArgumentCaptor<Collection<Cookie>> cookieCacheArgCaptor = ArgumentCaptor.forClass(Collection.class);
         verify(cookieCache, atLeastOnce()).addAll(cookieCacheArgCaptor.capture());
@@ -72,15 +64,11 @@ public class PersistentCookieJarTestWithTestDoubles {
     public void loadForRequest_WithMatchingUrl_ShouldReturnMatchingCookies() {
         Cookie savedCookie = TestCookieCreator.createNonPersistentCookie();
         CookieCache cookieCache = mock(CookieCache.class);
-        Iterator<Cookie> iterator = mock(Iterator.class);
-        when(iterator.hasNext()).thenReturn(true, false);
-        when(iterator.next()).thenReturn(savedCookie);
-        when(cookieCache.iterator()).thenReturn(iterator);
+        when(cookieCache.hasNext()).thenReturn(true, false);
+        when(cookieCache.next()).thenReturn(savedCookie);
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, mock(CookiePersistor.class));
 
-
         List<Cookie> requestCookies = persistentCookieJar.loadForRequest(TestCookieCreator.DEFAULT_URL);
-
 
         assertEquals(savedCookie, requestCookies.get(0));
     }
@@ -92,7 +80,7 @@ public class PersistentCookieJarTestWithTestDoubles {
         Iterator<Cookie> iterator = mock(Iterator.class);
         when(iterator.hasNext()).thenReturn(true, false);
         when(iterator.next()).thenReturn(savedCookie);
-        when(cookieCache.iterator()).thenReturn(iterator);
+//        when(cookieCache.iterator()).thenReturn(iterator);
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, mock(CookiePersistor.class));
 
         List<Cookie> requestCookies = persistentCookieJar.loadForRequest(TestCookieCreator.OTHER_URL);
@@ -106,7 +94,6 @@ public class PersistentCookieJarTestWithTestDoubles {
         Iterator<Cookie> iterator = mock(Iterator.class);
         when(iterator.hasNext()).thenReturn(true, false);
         when(iterator.next()).thenReturn(TestCookieCreator.createExpiredCookie());
-        when(cookieCache.iterator()).thenReturn(iterator);
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, mock(CookiePersistor.class));
 
         List<Cookie> cookies = persistentCookieJar.loadForRequest(TestCookieCreator.DEFAULT_URL);
@@ -118,18 +105,14 @@ public class PersistentCookieJarTestWithTestDoubles {
     public void loadForRequest_WithExpiredCookieMatchingUrl_ShouldRemoveTheCookie() {
         Cookie savedCookie = TestCookieCreator.createExpiredCookie();
         CookieCache cookieCache = mock(CookieCache.class);
-        Iterator<Cookie> iterator = mock(Iterator.class);
-        when(iterator.hasNext()).thenReturn(true, false);
-        when(iterator.next()).thenReturn(savedCookie);
-        when(cookieCache.iterator()).thenReturn(iterator);
+        when(cookieCache.hasNext()).thenReturn(true, false);
+        when(cookieCache.next()).thenReturn(savedCookie);
         CookiePersistor cookiePersistor = mock(CookiePersistor.class);
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, cookiePersistor);
 
-
         persistentCookieJar.loadForRequest(TestCookieCreator.DEFAULT_URL);
 
-
-        verify(iterator, times(1)).remove();
+        verify(cookieCache, times(1)).remove();
 
         ArgumentCaptor<Collection<Cookie>> cookiePersistorArgCaptor = ArgumentCaptor.forClass(Collection.class);
         verify(cookiePersistor).removeAll(cookiePersistorArgCaptor.capture());
@@ -145,11 +128,11 @@ public class PersistentCookieJarTestWithTestDoubles {
         persistentCookieJar.clearSession();
 
         verify(cookieCache, times(1)).clear();
-        verify(cookiePersistor,times(2)).loadAll();
+        verify(cookiePersistor, times(2)).loadAll();
     }
 
     @Test
-    public void clear_ShouldClearAllCookies(){
+    public void clear_ShouldClearAllCookies() {
         CookieCache cookieCache = mock(CookieCache.class);
         CookiePersistor cookiePersistor = mock(CookiePersistor.class);
         PersistentCookieJar persistentCookieJar = new PersistentCookieJar(cookieCache, cookiePersistor);
@@ -157,7 +140,6 @@ public class PersistentCookieJarTestWithTestDoubles {
         persistentCookieJar.clear();
 
         verify(cookieCache, times(1)).clear();
-        verify(cookiePersistor,times(1)).clear();
+        verify(cookiePersistor, times(1)).clear();
     }
-
 }
